52095bde2bf0a9ab534887b181bda25fc51b3359
QingpingMeng <missing1989@gmail.com>
2017-08-14 21:55:37 -0700

    adding lazy load compoment

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AppliedRoute from './components/AppliedRoute';
-import Home from './containers/Home';
-import NotFound from './containers/NotFound';
-import Login from './containers/Login';
-import Signup from './containers/Signup';
-import NewNote from './containers/NewNote';
-import Notes from './containers/Notes';
+import asyncComponent from './components/AsyncComponent';
 import AuthenticatedRoute from './components/AuthenticatedRoute';
 import UnauthenticatedRoute from './components/UnauthenticatedRoute';
 
-export default ({childProps}) => (
+const AsyncHome = asyncComponent(() => import('./containers/Home'));
+const AsyncNotFound = asyncComponent(() => import('./containers/NotFound'));
+const AsyncLogin = asyncComponent(() => import('./containers/Login'));
+const AsyncSignup = asyncComponent(() => import('./containers/Signup'));
+const AsyncNewNote = asyncComponent(() => import('./containers/NewNote'));
+const AsyncNotes = asyncComponent(() => import('./containers/Notes'));
+
+export default ({ childProps }) => (
     <Switch>
-        <AppliedRoute path='/' exact component={Home} props={childProps}/>
-        <UnauthenticatedRoute path='/Login' exact component={Login} props={childProps}/>
-        <UnauthenticatedRoute path='/Signup' exact component={Signup} props={childProps}/>
-        <AuthenticatedRoute path='/notes/new' exact component={NewNote} props={childProps} />
-        <AuthenticatedRoute path='/notes/:id' exact component={Notes} props={childProps} />
-        <AppliedRoute component={NotFound} />
+        <AppliedRoute path='/' exact component={AsyncHome} props={childProps} />
+        <UnauthenticatedRoute path='/Login' exact component={AsyncLogin} props={childProps} />
+        <UnauthenticatedRoute path='/Signup' exact component={AsyncSignup} props={childProps} />
+        <AuthenticatedRoute path='/notes/new' exact component={AsyncNewNote} props={childProps} />
+        <AuthenticatedRoute path='/notes/:id' exact component={AsyncNotes} props={childProps} />
+        <AppliedRoute component={AsyncNotFound} />
     </Switch>
 );
